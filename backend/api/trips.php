72c76595dc15d16500f4da8b17de08c461041f4a
MAXIMUS-TURBO <pixeldreamgirl@gmail.com>
2026-09-07 16:20:51 -0500

    backend php file for trips api to save trips

---
+++ backend/api/trips.php
@@ -0,0 +1,21 @@
+<!-- API endpoint for retrieving trip information -->
+<?php
+
+header('Content-Type: application/json');
+
+$trips = [
+    [
+        'id' => 1,
+        'destination' => 'Tokyo, Japan',
+        'startDate' => '2026-10-12',
+        'endDate' => '2026-10-20'
+    ],
+    [
+        'id' => 2,
+        'destination' => 'Paris, France',
+        'startDate' => '2027-03-15',
+        'endDate' => '2027-03-22'
+    ]
+];
+
+echo json_encode($trips);
